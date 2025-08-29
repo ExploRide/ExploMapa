0d33038985fca73b42cc227ec3359952b07cfd61
ExploRide <eksplorajder@gmail.com>
2025-08-30 00:20:31 +0200

    Update emoji-list.js

--- emoji-list.js
+++ emoji-list.js
@@ -45,6 +45,7 @@ const emojiList = [
   { id: "emoji44", url: "https://cdn.jsdelivr.net/gh/twitter/twemoji@latest/assets/svg/2755.svg" }, // ❕ białe !
   { id: "emoji45", url: "https://cdn.jsdelivr.net/gh/twitter/twemoji@latest/assets/svg/2757.svg" }, // ❗ czerwone !
   { id: "emoji46", url: "https://www.svgrepo.com/show/475670/pinterest-color.svg" }, // pinterest
-  { id: "emoji47", url: "https://cdn.jsdelivr.net/gh/twitter/twemoji@latest/assets/svg/1f69c.svg" }
+  { id: "emoji47", url: "https://cdn.jsdelivr.net/gh/twitter/twemoji@latest/assets/svg/1f69c.svg" }, // traktor
+{ id: "emoji48", url: "https://cdn.jsdelivr.net/gh/twitter/twemoji@latest/assets/svg/2692.svg" } // kopalnia
 
 ];
